4a97434a8865ba2340f6048581d838c445998de6
Mustafa <mushk45@gmail.com>
2013-05-08 18:35:44 -0700

    added triangles

--- Triangle.h
+++ Triangle.h
@@ -42,6 +42,93 @@ public:
         GeomObject::print_properties();
     }
 
+
+    void array2mat3(glm::mat3 &m, float nums[3*3]) {
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3; j++) {
+                m[j][i] = nums[i*3 + j];
+            }
+        }
+    }
+
+    virtual float intersect(const Ray &ray) {
+
+        // baycentric coords equations from Shirley book sec 10.3
+        
+        glm::mat3 A; // not for alpha. the matrix in denomenator
+        glm::mat3 B; // beta
+        glm::mat3 G; // gamma
+        glm::mat3 T; // for t
+
+        vec3 a = c1;
+        vec3 b = c2;
+        vec3 c = c3;
+/*
+#define a c1
+#define b c2
+#define c c3
+*/
+
+        float A_arr[] = {
+                   a.x - b.x, a.x - c.x, ray.d.x,
+                   a.y - b.y, a.y - c.y, ray.d.y,
+                   a.z - b.z, a.z - c.z, ray.d.z,
+                };
+
+        float B_arr[] = {
+                   a.x - ray.p0.x, a.x - c.x, ray.d.x,
+                   a.y - ray.p0.y, a.y - c.y, ray.d.y,
+                   a.z - ray.p0.z, a.z - c.z, ray.d.z,
+                };
+
+        float G_arr[] = {
+                   a.x - b.x, a.x - ray.p0.x, ray.d.x,
+                   a.y - b.y, a.y - ray.p0.y, ray.d.y,
+                   a.z - b.z, a.z - ray.p0.z, ray.d.z,
+                };
+
+        float T_arr[] = {
+                   a.x - b.x, a.x - c.x, a.x - ray.p0.x,
+                   a.y - b.y, a.y - c.y, a.y - ray.p0.y,
+                   a.z - b.z, a.z - c.z, a.z - ray.p0.z,
+                };
+
+        array2mat3(A, A_arr);
+        array2mat3(B, B_arr);
+        array2mat3(G, G_arr);
+        array2mat3(T, T_arr);
+
+        float A_det = glm::determinant(A);
+
+        // TODO - is this float cmp correct?
+        if (A_det == 0) {
+            return 0;
+        }
+
+        float beta, gamma, t;
+
+        t = glm::determinant(T) / A_det;
+        if (t < 0) {
+            return 0;
+        }
+
+        beta = glm::determinant(B) / A_det;
+        if (beta < 0 || beta > 1) {
+            return 0;
+        }
+
+        gamma = glm::determinant(G) / A_det;
+        if (gamma < 0 || gamma > 1 - beta) {
+            return 0;
+        }
+
+        return t;
+    }
+
+    vec3 getNormal(vec3 pos) {
+        return glm::normalize(glm::cross(c2-c1, c3-c1));
+    }
+
     vec3 c1;
     vec3 c2;
     vec3 c3;
--- match.cpp
+++ match.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 #include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 #include "util.h"
 
@@ -49,6 +50,37 @@ char *textFileRead(char *fn) {
 }
 
 
+// m[col][row]
+
+void zero_out_m4(glm::mat4 &m) {
+
+    for (int i=0; i<4; i++) {
+        for (int j=0; j<4; j++) {
+            m[i][j] = 0;
+        }
+    }
+}
+
+void get_m4(glm::mat4 &m, float nums[16]) {
+    for (int i=0; i<4; i++) {
+        for (int j=0; j<4; j++) {
+            m[j][i] = nums[i*4 + j];
+        }
+    }
+}
+
+void printm4(glm::mat4 m) {
+
+    for (int i=0; i<4; i++) {
+        for (int j=0; j<4; j++) {
+            cout << m[j][i] << " ";
+        }
+        cout << endl;
+    }
+}
+
+using glm::vec3;
+using glm::mat4;
 
 
 int main ()
@@ -62,16 +94,35 @@ int main ()
     v.z = 4;
 
     // v = glm::scale(v, vec3(5.0));
+    /*
     glm::vec3 u = glm::normalize(glm::vec3(5.0) * v);
-
     print3f(u, "u"); 
+    */
+
+
+    glm::mat4 m;
+
+    // m = glm::translate(mat4(1.0), vec3(4, 5, 6));
+
+    // zero_out_m4(m);
+
+    float nums[] = {1,2,3,4,
+               5,6,7,8,
+               9,10,8,9,
+               5,6,8,9,
+                };
+
+    get_m4(m, nums);
+
+
+    printm4(m);
+
+    // m[0][0] = 3;
     
     /*
     ifstream infile;
 
     infile.open("simple.pov", ifstream::in);
-
-    
 
     string s ("this subject has a submarine as a subsequence");
     smatch m;
